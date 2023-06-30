da6951a276122dcb0c7392e2b57a24181b6fbb60
TymoshchukMN <TymoshchukMN@gmail.com>
2023-06-30 15:06:03 +0300

    added objects for serialize

---
+++ TitleProcessing/DB/DBConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TitleProcessing.DB
+{
+    public class DBConfig
+    {
+        public string Server { get; set; }
+
+        public ushort Port { get; set; }
+
+        public ushort DBname { get; set; }
+
+        public ushort UsetName { get; set; }
+    }
+}
---
+++ TitleProcessing/Json/Config.cs
@@ -0,0 +1,9 @@
+﻿using TitleProcessing.DB;
+
+namespace TitleProcessing.Json
+{
+    public class Config
+    {
+        public DBConfig DataBaseConfig { get; set; }
+    }
+}
