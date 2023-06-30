9eb0f7349b310aa755d77b4e04591fc85cf3563a
TymoshchukMN <TymoshchukMN@gmail.com>
2023-06-30 16:41:33 +0300

    added class VerifiableDB

---
+++ TitleProcessing/DB/VerifiableDB.cs
@@ -0,0 +1,12 @@
+﻿namespace TitleProcessing.DB
+{
+    public static class VerifiableDB
+    {
+        static readonly string[] VerifiableDBValue = {
+            "1C7Dymerka",
+            "1C7Shops",
+            "1C7Torg",
+            "Zoom",
+        };
+    }
+}
